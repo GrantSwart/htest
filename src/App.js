1ee11bea879bbb6d7fee9c25d995d22b25ad3061
Grant <grant@lite-support.co.uk>
2019-10-18 11:52:47 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,74 +1,76 @@
-import React from "react";
+import React, { Component } from "react";
 import Clock from "./components/Clock";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="outerbox">
-          <div className="box" id="theleftcontent">
-            <div className="hoverbox">
-              <div className="innerbox" id="title">
-                London
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="outerbox">
+            <div className="box" id="theleftcontent">
+              <div className="hoverbox">
+                <div className="innerbox" id="title">
+                  London
+                </div>
+                <Clock timezone={"UTC-0"} city={"London"} offset={"0"} />
               </div>
-              <Clock timezone={"UTC-0"} city={"London"} offset={"0"} />
             </div>
           </div>
-        </div>
-        <div className="outerbox">
-          <div className="box" id="therightcontent">
-            <div className="hoverbox">
-              <div className="innerbox" id="title">
-                Auckland
+          <div className="outerbox">
+            <div className="box" id="therightcontent">
+              <div className="hoverbox">
+                <div className="innerbox" id="title">
+                  Auckland
+                </div>
+                <Clock timezone={"UTC-13"} city={"Auckland"} offset={"13"} />
               </div>
-              <Clock timezone={"UTC-13"} city={"Auckland"} offset={"13"} />
             </div>
           </div>
-        </div>
-        <div className="outerbox">
-          <div className="box" id="therightcontent">
-            <div className="hoverbox">
-              <div className="innerbox" id="title">
-                Edmonton
+          <div className="outerbox">
+            <div className="box" id="therightcontent">
+              <div className="hoverbox">
+                <div className="innerbox" id="title">
+                  Edmonton
+                </div>
+                <Clock timezone={"UTC-6"} city={"Edmonton"} offset={"6"} />
               </div>
-              <Clock timezone={"UTC-6"} city={"Edmonton"} offset={"6"} />
             </div>
           </div>
-        </div>
-        <div className="outerbox">
-          <div className="box" id="therightcontent">
-            <div className="hoverbox">
-              <div className="innerbox" id="title">
-                Buenos Aires
+          <div className="outerbox">
+            <div className="box" id="therightcontent">
+              <div className="hoverbox">
+                <div className="innerbox" id="title">
+                  Buenos Aires
+                </div>
+                <Clock timezone={"UTC-3"} city={"Buenos Aires"} offset={"3"} />
               </div>
-              <Clock timezone={"UTC-3"} city={"Buenos Aires"} offset={"3"} />
             </div>
           </div>
-        </div>
-        <div className="outerbox">
-          <div className="box" id="therightcontent">
-            <div className="hoverbox">
-              <div className="innerbox" id="title">
-                Cape Town
+          <div className="outerbox">
+            <div className="box" id="therightcontent">
+              <div className="hoverbox">
+                <div className="innerbox" id="title">
+                  Cape Town
+                </div>
+                <Clock timezone={"UTC-2"} city={"Cape Town"} offset={"2"} />
               </div>
-              <Clock timezone={"UTC-2"} city={"Cape Town"} offset={"2"} />
             </div>
           </div>
-        </div>
-        <div className="outerbox">
-          <div className="box" id="therightcontent">
-            <div className="hoverbox">
-              <div className="innerbox" id="title">
-                Los Angeles
+          <div className="outerbox">
+            <div className="box" id="therightcontent">
+              <div className="hoverbox">
+                <div className="innerbox" id="title">
+                  Los Angeles
+                </div>
+                <Clock timezone={"UTC-7"} city={"Los Angeles"} offset={"-7"} />
               </div>
-              <Clock timezone={"UTC-7"} city={"Los Angeles"} offset={"-7"} />
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
